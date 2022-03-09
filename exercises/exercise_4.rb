e73be0ca548d3306c4edcb48389bcbfb4ae78744
Hongling Zhang <honglingzhang@Honglings-MacBook-Pro.local>
2022-03-08 22:14:52 -0700

    Select stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name:"Burnaby",annual_revenue:300000,mens_apparel:true,womens_apparel:true)
+Store.create(name:"Richmond",annual_revenue:1260000,mens_apparel:false,womens_apparel:true)
+Store.create(name:"Gastown",annual_revenue:190000,mens_apparel:true,womens_apparel:false)
+
+@mens_stores = Store.where(mens_apparel:true)
+@mens_stores.each do |men|
+  puts men.name, men.annual_revenue
+end
+
+@womens_stores = Store.where(womens_apparel:true, annual_revenue:0..1000000)
+@womens_stores.each do |women|
+  puts women.name, women.annual_revenue
+end
