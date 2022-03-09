a99486ef4a65ed8e7481577c372904c94163ecde
Hongling Zhang <honglingzhang@Honglings-MacBook-Pro.local>
2022-03-08 23:29:01 -0700

    Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,30 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { in: 40..200 }
+  validates :store_id, presence: true
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than:0}
+  validate :must_have_men_or_women_apparel
+  def must_have_men_or_women_apparel
+    if mens_apparel?
+      errors.add(:mens_apparel, "can't have any apparel") 
+    end
+
+    if womens_apparel?
+      errors.add(:womens_apparel, "can't have any apparel") 
+    end
+  end
+end
+
+
+puts "please input a store's name"
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+puts new_store.errors.full_messages
